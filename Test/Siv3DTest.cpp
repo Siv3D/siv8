bbb2aa50ef17b58e3d1adf6c6a7d4f81a620d25c
Ryo Suzuki <reputeless+github@gmail.com>
2022-09-16 23:16:34 +0900

    update

--- Test/Siv3DTest.cpp
+++ Test/Siv3DTest.cpp
@@ -9,6 +9,12 @@
 //
 //-----------------------------------------------
 
+# define ANKERL_NANOBENCH_IMPLEMENT
+# include <ThirdParty/nanobench/nanobench.h>
+
+# define DOCTEST_CONFIG_IMPLEMENT
+# include <ThirdParty/doctest/doctest.h>
+
 # include "Siv3DTest.hpp"
 
 void TestPlatform();
@@ -18,6 +24,39 @@ void RunTest()
 	Console.open();
 
 	TestPlatform();
+
+	doctest::Context context;
+
+	context.run();
+}
+
+TEST_CASE("Types.hpp")
+{
+	CHECK(sizeof(int8) == 1);
+	CHECK(sizeof(uint8) == 1);
+	CHECK(sizeof(int16) == 2);
+	CHECK(sizeof(uint16) == 2);
+	CHECK(sizeof(int32) == 4);
+	CHECK(sizeof(uint32) == 4);
+	CHECK(sizeof(int64) == 8);
+	CHECK(sizeof(uint64) == 8);
+	CHECK(sizeof(char8) == 1);
+	CHECK(sizeof(char16) == 2);
+	CHECK(sizeof(char32) == 4);
+}
+
+TEST_CASE("Unicode.hpp")
+{
+	CHECK(Unicode::WidenAscii("") == U"");
+	CHECK(Unicode::WidenAscii("abc!#@") == U"abc!#@");
+
+	CHECK(Unicode::Widen("") == U"");
+	CHECK(Unicode::Widen("abc!#@") == U"abc!#@");
+	CHECK(Unicode::Widen("渋三次元") == U"渋三次元");
+
+	CHECK(Unicode::FromWstring(L"") == U"");
+	CHECK(Unicode::FromWstring(L"abc!#@") == U"abc!#@");
+	CHECK(Unicode::FromWstring(L"渋三次元") == U"渋三次元");
 }
 
 void TestPlatform()
