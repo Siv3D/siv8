a149b91a65daf9de9f54b7c5f617a316777d1d20
Ryo Suzuki <reputeless+github@gmail.com>
2024-03-08 01:21:26 +0900

    Color

---
+++ Siv3D/src/Siv3D/Color/SivColor.cpp
@@ -0,0 +1,75 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2024 Ryo Suzuki
+//	Copyright (c) 2016-2024 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/ColorHSV.hpp>
+# include <Siv3D/IntFormatter.hpp>
+
+namespace s3d
+{
+	String Color::toHex() const
+	{
+		const uint32 rgb = ((r << 16) | (g << 8) | b);
+		return U"{:0>6X}"_fmt(rgb);
+	}
+
+	////////////////////////////////////////////////////////////////
+	//
+	//	Formatter
+	//
+	////////////////////////////////////////////////////////////////
+
+	void Formatter(FormatData& formatData, const Color& value)
+	{
+		constexpr size_t BufferSize = ((3 * 4) + 8 + 1);
+		char32 buffer[BufferSize];
+		char32* p = buffer;
+
+		*(p++) = U'(';
+		detail::AppendInt32(&p, value.r);
+		*(p++) = U',';
+		*(p++) = U' ';
+		detail::AppendInt32(&p, value.g);
+		*(p++) = U',';
+		*(p++) = U' ';
+		detail::AppendInt32(&p, value.b);
+		*(p++) = U',';
+		*(p++) = U' ';
+		detail::AppendInt32(&p, value.a);
+		*(p++) = U')';
+
+		formatData.string.append(buffer, (p - buffer));
+	}
+}
+
+////////////////////////////////////////////////////////////////
+//
+//	fmt
+//
+////////////////////////////////////////////////////////////////
+
+s3d::ParseContext::iterator fmt::formatter<s3d::Color, s3d::char32>::parse(s3d::ParseContext& ctx)
+{
+	return s3d::FmtHelper::GetFormatTag(tag, ctx);
+}
+
+s3d::BufferContext::iterator fmt::formatter<s3d::Color, s3d::char32>::format(const s3d::Color& value, s3d::BufferContext& ctx)
+{
+	if (tag.empty())
+	{
+		return format_to(ctx.out(), U"({}, {}, {}, {})", value.r, value.g, value.b, value.a);
+	}
+	else
+	{
+		const std::u32string format
+			= (U"({:" + tag + U"}, {:" + tag + U"}, {:" + tag + U"}, {:" + tag + U"})");
+		return format_to(ctx.out(), format, value.r, value.g, value.b, value.a);
+	}
+}
