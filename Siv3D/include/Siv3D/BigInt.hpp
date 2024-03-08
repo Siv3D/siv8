d36f27163cbd14ba361361efd6a186a8813445df
Ryo Suzuki <reputeless+github@gmail.com>
2024-03-09 00:12:45 +0900

    Update BigInt.hpp

--- Siv3D/include/Siv3D/BigInt.hpp
+++ Siv3D/include/Siv3D/BigInt.hpp
@@ -64,11 +64,713 @@ namespace s3d
 
 		~BigInt();
 
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator =
+		//
+		////////////////////////////////////////////////////////////////
 
+		BigInt& operator =(int64 i);
+		
+		BigInt& operator =(uint64 i);
+		
+		BigInt& operator =(Concept::SignedIntegral auto i);
 
+		BigInt& operator =(Concept::UnsignedIntegral auto i);
 
+		BigInt& operator =(const BigInt& other);
+		
+		BigInt& operator =(BigInt&& other) noexcept;
+		
+		BigInt& operator =(std::string_view number);
+		
+		BigInt& operator =(StringView number);
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator +
+		//
+		////////////////////////////////////////////////////////////////
 
+		[[nodiscard]]
+		const BigInt& operator +() const;
 
+		[[nodiscard]]
+		BigInt operator +(int64 i) const;
+		
+		[[nodiscard]]
+		BigInt operator +(uint64 i) const;
+
+		[[nodiscard]]
+		BigInt operator +(Concept::SignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator +(Concept::UnsignedIntegral auto i) const;
+		
+		[[nodiscard]]
+		BigInt operator +(const BigInt& i) const;
+
+		[[nodiscard]]
+		friend BigInt operator +(const Concept::Integral auto a, const BigInt& b)
+		{
+			return (b + a);
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator -
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		BigInt operator -() const&;
+		
+		[[nodiscard]]
+		BigInt operator -()&&;
+
+		[[nodiscard]]
+		BigInt operator -(int64 i) const;
+		
+		[[nodiscard]]
+		BigInt operator -(uint64 i) const;
+
+		[[nodiscard]]
+		BigInt operator -(Concept::SignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator -(Concept::UnsignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator -(const BigInt& i) const;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator *
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		BigInt operator *(int64 i) const;
+
+		[[nodiscard]]
+		BigInt operator *(uint64 i) const;
+
+		[[nodiscard]]
+		BigInt operator *(Concept::SignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator *(Concept::UnsignedIntegral auto i) const;
+		
+		[[nodiscard]]
+		BigInt operator *(const BigInt& i) const;
+
+		[[nodiscard]]
+		friend BigInt operator *(Concept::Integral auto a, const BigInt& b)
+		{
+			return (b * a);
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator /
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]		
+		BigInt operator /(int64 i) const;
+
+		[[nodiscard]]
+		BigInt operator /(uint64 i) const;
+
+		[[nodiscard]]
+		BigInt operator /(Concept::SignedIntegral auto i) const;
+		
+		[[nodiscard]]
+		BigInt operator /(Concept::UnsignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator /(const BigInt& i) const;
+
+		[[nodiscard]]
+		friend BigInt operator /(Concept::SignedIntegral auto a, const BigInt& b)
+		{
+			return (static_cast<int64>(a) / b);
+		}
+
+		[[nodiscard]]
+		friend BigInt operator /(const Concept::UnsignedIntegral auto a, const BigInt& b)
+		{
+			return (static_cast<uint64>(a) / b);
+		}
+
+		[[nodiscard]]
+		friend BigInt operator /(const int64 a, const BigInt& b)
+		{
+			return b._divI(a);
+		}
+
+		[[nodiscard]]
+		friend BigInt operator /(const uint64 a, const BigInt& b)
+		{
+			return b._divI(a);
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator %
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]		
+		BigInt operator %(int64 i) const;
+
+		[[nodiscard]]
+		BigInt operator %(uint64 i) const;
+
+		[[nodiscard]]
+		BigInt operator %(Concept::SignedIntegral auto i) const;
+		
+		[[nodiscard]]
+		BigInt operator %(Concept::UnsignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator %(const BigInt& i) const;
+
+		[[nodiscard]]
+		friend BigInt operator %(Concept::SignedIntegral auto a, const BigInt& b)
+		{
+			return (static_cast<int64>(a) % b);
+		}
+
+		[[nodiscard]]
+		friend BigInt operator %(const Concept::UnsignedIntegral auto a, const BigInt& b)
+		{
+			return (static_cast<uint64>(a) % b);
+		}
+
+		[[nodiscard]]
+		friend BigInt operator %(const int64 a, const BigInt& b)
+		{
+			return b._modI(a);
+		}
+
+		[[nodiscard]]
+		friend BigInt operator %(const uint64 a, const BigInt& b)
+		{
+			return b._modI(a);
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator +=
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator +=(int64 i);
+		
+		BigInt& operator +=(uint64 i);
+
+		BigInt& operator +=(Concept::SignedIntegral auto i);
+
+		BigInt& operator +=(Concept::UnsignedIntegral auto i);
+		
+		BigInt& operator +=(const BigInt& i);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator -=
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator -=(int64 i);
+		
+		BigInt& operator -=(uint64 i);
+
+		BigInt& operator -=(Concept::SignedIntegral auto i);
+
+		BigInt& operator -=(Concept::UnsignedIntegral auto i);
+		
+		BigInt& operator -=(const BigInt& i);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator *=
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator *=(int64 i);
+		
+		BigInt& operator *=(uint64 i);
+
+		BigInt& operator *=(Concept::SignedIntegral auto i);
+
+		BigInt& operator *=(Concept::UnsignedIntegral auto i);
+		
+		BigInt& operator *=(const BigInt& i);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator /=
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator /=(int64 i);
+		
+		BigInt& operator /=(uint64 i);
+
+		BigInt& operator /=(Concept::SignedIntegral auto i);
+
+		BigInt& operator /=(Concept::UnsignedIntegral auto i);
+		
+		BigInt& operator /=(const BigInt& i);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator %=
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator %=(int64 i);
+		
+		BigInt& operator %=(uint64 i);
+
+		BigInt& operator %=(Concept::SignedIntegral auto i);
+
+		BigInt& operator %=(Concept::UnsignedIntegral auto i);
+		
+		BigInt& operator %=(const BigInt& i);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator ++
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator ++()&;
+
+		BigInt operator ++()&&;
+
+		BigInt operator ++(int);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator --
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator --()&;
+
+		BigInt operator --()&&;
+		
+		BigInt operator --(int);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator &
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		BigInt operator &(int64 i) const;
+
+		[[nodiscard]]
+		BigInt operator &(uint64 i) const;
+
+		[[nodiscard]]
+		BigInt operator &(Concept::SignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator &(Concept::UnsignedIntegral auto i) const;
+		
+		[[nodiscard]]
+		BigInt operator &(const BigInt& i) const;
+
+		[[nodiscard]]
+		friend BigInt operator &(Concept::Integral auto a, const BigInt& b)
+		{
+			return (b & a);
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator |
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		BigInt operator |(int64 i) const;
+
+		[[nodiscard]]
+		BigInt operator |(uint64 i) const;
+
+		[[nodiscard]]
+		BigInt operator |(Concept::SignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator |(Concept::UnsignedIntegral auto i) const;
+		
+		[[nodiscard]]
+		BigInt operator |(const BigInt& i) const;
+
+		[[nodiscard]]
+		friend BigInt operator |(Concept::Integral auto a, const BigInt& b)
+		{
+			return (b | a);
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator ^
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		BigInt operator ^(int64 i) const;
+
+		[[nodiscard]]
+		BigInt operator ^(uint64 i) const;
+
+		[[nodiscard]]
+		BigInt operator ^(Concept::SignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator ^(Concept::UnsignedIntegral auto i) const;
+		
+		[[nodiscard]]
+		BigInt operator ^(const BigInt& i) const;
+
+		[[nodiscard]]
+		friend BigInt operator ^(Concept::Integral auto a, const BigInt& b)
+		{
+			return (b ^ a);
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator &=
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator &=(int64 i);
+		
+		BigInt& operator &=(uint64 i);
+
+		BigInt& operator &=(Concept::SignedIntegral auto i);
+
+		BigInt& operator &=(Concept::UnsignedIntegral auto i);
+		
+		BigInt& operator &=(const BigInt& i);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator |=
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator |=(int64 i);
+		
+		BigInt& operator |=(uint64 i);
+
+		BigInt& operator |=(Concept::SignedIntegral auto i);
+
+		BigInt& operator |=(Concept::UnsignedIntegral auto i);
+		
+		BigInt& operator |=(const BigInt& i);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator ^=
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator ^=(int64 i);
+		
+		BigInt& operator ^=(uint64 i);
+
+		BigInt& operator ^=(Concept::SignedIntegral auto i);
+
+		BigInt& operator ^=(Concept::UnsignedIntegral auto i);
+		
+		BigInt& operator ^=(const BigInt& i);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator <<
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		BigInt operator <<(int64 i) const;
+		
+		[[nodiscard]]
+		BigInt operator <<(uint64 i) const;
+
+		[[nodiscard]]
+		BigInt operator <<(Concept::SignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator <<(Concept::UnsignedIntegral auto i) const;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator >>
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		BigInt operator >>(int64 i) const;
+		
+		[[nodiscard]]
+		BigInt operator >>(uint64 i) const;
+
+		[[nodiscard]]
+		BigInt operator >>(Concept::SignedIntegral auto i) const;
+
+		[[nodiscard]]
+		BigInt operator >>(Concept::UnsignedIntegral auto i) const;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator <<=
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator <<=(int64 i);
+		
+		BigInt& operator <<=(uint64 i);
+		
+		BigInt& operator <<=(Concept::SignedIntegral auto i);
+
+		BigInt& operator <<=(Concept::UnsignedIntegral auto i);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator >>=
+		//
+		////////////////////////////////////////////////////////////////
+
+		BigInt& operator >>=(int64 i);
+		
+		BigInt& operator >>=(uint64 i);
+		
+		BigInt& operator >>=(Concept::SignedIntegral auto i);
+
+		BigInt& operator >>=(Concept::UnsignedIntegral auto i);
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator ==
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		friend bool operator ==(const BigInt& a, const BigInt& b) noexcept
+		{
+			return (a.compare(b) == 0);
+		}
+
+		[[nodiscard]]
+		friend bool operator ==(const BigInt& a, const Concept::Arithmetic auto b) noexcept
+		{
+			return (a.compare(b) == 0);
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator <=>
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		friend auto operator <=>(const BigInt& a, const BigInt& b) noexcept
+		{
+			return (a.compare(b) <=> 0);
+		}
+
+		[[nodiscard]]
+		friend auto operator <=>(const BigInt& a, const Concept::Arithmetic auto b) noexcept
+		{
+			return (a.compare(b) <=> 0);
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator bool
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		explicit operator bool() const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	isZero
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		bool isZero() const noexcept;
+	
+		////////////////////////////////////////////////////////////////
+		//
+		//	sign
+		//
+		////////////////////////////////////////////////////////////////
+	
+		[[nodiscard]]
+		int32 sign() const noexcept;
+	
+		////////////////////////////////////////////////////////////////
+		//
+		//	abs
+		//
+		////////////////////////////////////////////////////////////////
+	
+		[[nodiscard]]
+		BigInt abs() const;
+	
+		////////////////////////////////////////////////////////////////
+		//
+		//	pow
+		//
+		////////////////////////////////////////////////////////////////
+	
+		[[nodiscard]]
+		BigInt pow(uint32 x) const;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	divmod
+		//
+		////////////////////////////////////////////////////////////////
+		
+		void divmod(const BigInt& x, BigInt& q, BigInt& r) const;
+	
+		////////////////////////////////////////////////////////////////
+		//
+		//	lsb
+		//
+		////////////////////////////////////////////////////////////////
+	
+		[[nodiscard]]
+		uint32 lsb() const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	bitTest
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		bool bitTest(uint32 index) const noexcept;
+	
+		////////////////////////////////////////////////////////////////
+		//
+		//	bitSet
+		//
+		////////////////////////////////////////////////////////////////
+	
+		BigInt& bitSet(uint32 index, bool value) noexcept;
+	
+		////////////////////////////////////////////////////////////////
+		//
+		//	bitFlip
+		//
+		////////////////////////////////////////////////////////////////
+	
+		BigInt& bitFlip(uint32 index) noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	swap
+		//
+		////////////////////////////////////////////////////////////////
+
+		void swap(BigInt& other) noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	hash
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		size_t hash() const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	asInt32, asUint32
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		int32 asInt32() const noexcept;
+		
+		[[nodiscard]]
+		uint32 asUint32() const noexcept;
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	asInt64, asUint64
+		//
+		////////////////////////////////////////////////////////////////
+	
+		[[nodiscard]]
+		int64 asInt64() const noexcept;
+	
+		[[nodiscard]]
+		uint64 asUint64() const noexcept;
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	asFloat, asDouble
+		//
+		////////////////////////////////////////////////////////////////
+	
+		[[nodiscard]]
+		float asFloat() const noexcept;
+		
+		[[nodiscard]]
+		double asDouble() const noexcept;
+			
+		////////////////////////////////////////////////////////////////
+		//
+		//	asBigFloat
+		//
+		////////////////////////////////////////////////////////////////
+		
+		//[[nodiscard]]
+		//BigFloat asBigFloat() const;
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator size_t
+		//
+		////////////////////////////////////////////////////////////////
+	
+		[[nodiscard]]
+		explicit operator size_t() const noexcept;
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator float, operator double
+		//
+		////////////////////////////////////////////////////////////////
+	
+		[[nodiscard]]
+		explicit operator float() const noexcept;
+
+		[[nodiscard]]
+		explicit operator double() const noexcept;
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator BigFloat
+		//
+		////////////////////////////////////////////////////////////////
+	
+		//[[nodiscard]]
+		//explicit operator BigFloat() const;
 
 		////////////////////////////////////////////////////////////////
 		//
@@ -88,10 +790,72 @@ namespace s3d
 		[[nodiscard]]
 		String str() const;
 
+		////////////////////////////////////////////////////////////////
+		//
+		//	compare
+		//
+		////////////////////////////////////////////////////////////////
 
+		[[nodiscard]]
+		int32 compare(int64 i) const noexcept;
+		
+		[[nodiscard]]
+		int32 compare(uint64 i) const noexcept;
+		
+		[[nodiscard]]
+		int32 compare(long double f) const noexcept;
 
+		[[nodiscard]]
+		int32 compare(Concept::SignedIntegral auto i) const noexcept;
 
+		[[nodiscard]]
+		int32 compare(Concept::UnsignedIntegral auto i) const noexcept;
 
+		[[nodiscard]]
+		int32 compare(Concept::FloatingPoint auto f) const noexcept;
+		
+		[[nodiscard]]
+		int32 compare(const BigInt& i) const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	swap
+		//
+		////////////////////////////////////////////////////////////////
+
+		friend void swap(BigInt& lhs, BigInt& rhs) noexcept
+		{
+			lhs.swap(rhs);
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator <<
+		//
+		////////////////////////////////////////////////////////////////
+
+		template <class CharType>
+		friend std::basic_ostream<CharType>& operator <<(std::basic_ostream<CharType>& output, const BigInt& value)
+		{
+			return (output << value.str());
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator >>
+		//
+		////////////////////////////////////////////////////////////////
+
+		template <class CharType>
+		friend std::basic_istream<CharType>& operator >>(std::basic_istream<CharType>& input, BigInt& value)
+		{
+			if (String s; input >> s)
+			{
+				value = s;
+			}
+
+			return input;
+		}
 
 		////////////////////////////////////////////////////////////////
 		//
@@ -106,7 +870,28 @@ namespace s3d
 		struct BigIntDetail;
 
 		std::unique_ptr<BigIntDetail> pImpl;
+
+		BigInt _divI(int64 a) const;
+		BigInt _divI(uint64 a) const;
+		BigInt _modI(int64 a) const;
+		BigInt _modI(uint64 a) const;
 	};
 }
+
+////////////////////////////////////////////////////////////////
+//
+//	std::hash
+//
+////////////////////////////////////////////////////////////////
+
+template <>
+struct std::hash<s3d::BigInt>
+{
+	[[nodiscard]]
+	size_t operator ()(const s3d::BigInt& value) const noexcept
+	{
+		return value.hash();
+	}
+};
 
 # include "detail/BigInt.ipp"
