0d22b74f7611451e043747c862d1277cb239e1f4
Ryo Suzuki <reputeless+github@gmail.com>
2024-03-10 21:05:42 +0900

    RandomDuration

--- Siv3D/include/Siv3D.hpp
+++ Siv3D/include/Siv3D.hpp
@@ -139,7 +139,7 @@
 # include <Siv3D/StringView.hpp>
 
 // 文字列 | String
-# include <Siv3D/String.hpp> // ToDo
+# include <Siv3D/String.hpp>
 
 // Unicode | Unicode
 # include <Siv3D/Unicode.hpp>
@@ -310,9 +310,9 @@
 
 //// 多倍長浮動小数点数 | 100 decimal digits precision floating point type
 //# include <Siv3D/BigFloat.hpp>
-//
-//// 浮動小数点数型の共通型 | Common Float
-//# include <Siv3D/CommonFloat.hpp>
+
+// 浮動小数点数型の共通型 | Common Float
+# include <Siv3D/CommonFloat.hpp>
 
 ////////////////////////////////////////////////////////////////
 //
@@ -640,9 +640,9 @@
 // シャッフル | Random shuffle
 # include <Siv3D/Shuffle.hpp>
 
-//// ランダムな時間 | Random duration
-//# include <Siv3D/RandomDuration.hpp>
-//
+// ランダムな時間 | Random duration
+# include <Siv3D/RandomDuration.hpp>
+
 //// ランダムな Point の作成 | Random Point generation
 //# include <Siv3D/RandomPoint.hpp>
 //
@@ -684,9 +684,6 @@
 
 // 4 次元ベクトル | 4D vector
 # include <Siv3D/Vector4D.hpp> // ToDo
-
-// 浮動小数点数の計算補助 | Floating point support
-# include <Siv3D/CommonFloat.hpp>
 
 //// ベクトルクラステンプレートの計算補助 | Vector class templates support
 //# include <Siv3D/CommonVector.hpp>
---
+++ Siv3D/include/Siv3D/RandomDuration.hpp
@@ -0,0 +1,39 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2024 Ryo Suzuki
+//	Copyright (c) 2016-2024 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include "Common.hpp"
+# include "Random.hpp"
+# include "Duration.hpp"
+
+namespace s3d
+{
+	/// @brief 現在のスレッドの乱数エンジンを用いて、0 以上 max 以下の範囲の時間を生成して返します。
+	/// @param max 生成する時間の最大値
+	/// @return 生成されたランダムな時間
+	[[nodiscard]]
+	Duration Random(const Duration& max) noexcept;
+
+	[[nodiscard]]
+	Duration Random(const Duration& max, Concept::UniformRandomBitGenerator auto&& urbg) noexcept;
+
+	/// @brief 現在のスレッドの乱数エンジンを用いて、min 以上 max 以下の範囲の時間を生成して返します。
+	/// @param min 生成する時間の最小値
+	/// @param max 生成する時間の最大値
+	/// @return 生成されたランダムな時間
+	[[nodiscard]]
+	Duration Random(const Duration& min, const Duration& max) noexcept;
+
+	[[nodiscard]]
+	Duration Random(const Duration& min, const Duration& max, Concept::UniformRandomBitGenerator auto&& urbg) noexcept;
+}
+
+# include "detail/RandomDuration.ipp"
