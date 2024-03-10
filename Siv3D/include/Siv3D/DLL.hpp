340f001145c1a01eb484b8463a7ad5cde6d2e62b
Ryo Suzuki <reputeless+github@gmail.com>
2024-03-10 20:44:01 +0900

    DLL

--- Siv3D/include/Siv3D/DLL.hpp
+++ Siv3D/include/Siv3D/DLL.hpp
@@ -10,3 +10,122 @@
 //-----------------------------------------------
 
 # pragma once
+# include "Common.hpp"
+# include "StringView.hpp"
+
+# if SIV3D_PLATFORM(WINDOWS)
+
+# define NOGDI
+# include <Siv3D/Windows/Windows.hpp>
+
+namespace s3d
+{
+	using LibraryHandle = HMODULE;
+
+	namespace DLL
+	{
+		[[nodiscard]]
+		LibraryHandle LoadSystemLibraryNoThrow(const wchar_t* library);
+
+		[[nodiscard]]
+		LibraryHandle LoadSystemLibrary(const wchar_t* library);
+
+		[[nodiscard]]
+		LibraryHandle Load(StringView path);
+
+		void Unload(LibraryHandle& library);
+
+		class GetFunctionNoThrow
+		{
+		public:
+
+			[[nodiscard]]
+			GetFunctionNoThrow(LibraryHandle _module, const char* name);
+
+			template <class Type>
+			[[nodiscard]]
+			operator Type() const noexcept
+			{
+				return static_cast<Type>(p);
+			}
+
+		private:
+
+			const void* p = nullptr;
+		};
+
+		class GetFunction
+		{
+		public:
+
+			[[nodiscard]]
+			GetFunction(LibraryHandle _module, const char* name);
+
+			template <class Type>
+			[[nodiscard]]
+			operator Type() const noexcept
+			{
+				return static_cast<Type>(p);
+			}
+
+		private:
+
+			const void* p = nullptr;
+		};
+	}
+}
+
+# else
+
+# include <dlfcn.h>
+
+namespace s3d
+{
+	using LibraryHandle = void*;
+
+	namespace DLL
+	{
+		[[nodiscard]]
+		LibraryHandle Load(StringView path);
+	
+		void Unload(LibraryHandle& library);
+
+		class GetFunctionNoThrow
+		{
+		public:
+
+			[[nodiscard]]
+			GetFunctionNoThrow(LibraryHandle _module, const char* name);
+
+			template <class Type>
+			operator Type() const noexcept
+			{
+				return reinterpret_cast<Type>(p);
+			}
+
+		private:
+
+			void* p;
+		};
+
+		class GetFunction
+		{
+		public:
+
+			[[nodiscard]]
+			GetFunction(LibraryHandle _module, const char* name);
+
+			template <class Type>
+			operator Type() const noexcept
+			{
+				return reinterpret_cast<Type>(p);
+			}
+
+		private:
+
+			void* p = nullptr;
+		};
+	}
+}
+
+# endif
---
+++ Siv3D/src/Siv3D-Platform/WindowsDesktop/Siv3D/DLL/SivDLL.cpp
@@ -0,0 +1,78 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2024 Ryo Suzuki
+//	Copyright (c) 2016-2024 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/DLL.hpp>
+# include <Siv3D/Error/InternalEngineError.hpp>
+# include <Siv3D/Unicode.hpp>
+# include <Siv3D/EngineLog.hpp>
+
+namespace s3d
+{
+	namespace DLL
+	{
+		LibraryHandle LoadSystemLibraryNoThrow(const wchar_t* library)
+		{
+			LOG_DEBUG(fmt::format("DLL::LoadSystemLibraryNoThrow(\"{0}\")", Unicode::FromWstring(library)));
+
+			return ::LoadLibraryExW(library, nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+		}
+
+		LibraryHandle LoadSystemLibrary(const wchar_t* library)
+		{
+			LOG_DEBUG(fmt::format("DLL::LoadSystemLibrary(\"{0}\")", Unicode::FromWstring(library)));
+
+			const LibraryHandle hModule = ::LoadLibraryExW(library, nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+
+			if (not hModule)
+			{
+				throw InternalEngineError{ fmt::format("Failed to load `{}`", Unicode::FromWstring(library)) };
+			}
+
+			return hModule;
+		}
+
+		LibraryHandle Load(const StringView path)
+		{
+			LOG_TRACE(fmt::format("DLL::Load(\"path = `{}`\")", path));
+
+			return ::LoadLibraryW(path.toWstr().c_str());
+		}
+
+		void Unload(LibraryHandle& library)
+		{
+			if (not library)
+			{
+				return;
+			}
+
+			::FreeLibrary(library);
+
+			library = nullptr;
+		}
+
+		GetFunctionNoThrow::GetFunctionNoThrow(LibraryHandle _module, const char* name)
+			: p{ static_cast<const void*>(::GetProcAddress(_module, name)) }
+		{
+			LOG_DEBUG(fmt::format("DLL::GetFunctionNoThrow::GetFunctionNoThrow(name = \"{0}\") p = {1}", name, p));
+		}
+
+		GetFunction::GetFunction(LibraryHandle _module, const char* name)
+			: p(static_cast<const void*>(::GetProcAddress(_module, name)))
+		{
+			LOG_DEBUG(fmt::format("DLL::GetFunction::GetFunction(name = \"{0}\") p = {1}", name, p));
+
+			if (not p)
+			{
+				throw InternalEngineError{ fmt::format("Failed to get function: `{}`", name) };
+			}
+		}
+	}
+}
