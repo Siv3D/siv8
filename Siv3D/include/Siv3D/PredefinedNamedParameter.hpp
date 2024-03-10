152c507300286b0cb6afc86f074b4ea9fffe97af
Ryo Suzuki <reputeless+github@gmail.com>
2024-03-10 22:36:30 +0900

    Rect

--- Siv3D/include/Siv3D/PredefinedNamedParameter.hpp
+++ Siv3D/include/Siv3D/PredefinedNamedParameter.hpp
@@ -17,4 +17,18 @@ namespace s3d::Arg
 	SIV3D_NAMED_PARAMETER(generator);			// Array, Image
 	SIV3D_NAMED_PARAMETER(indexedGenerator);	// Array
 	SIV3D_NAMED_PARAMETER(reserve);				// Array
+
+	SIV3D_NAMED_PARAMETER(center);				// Rect, RectF, Circle
+	SIV3D_NAMED_PARAMETER(topLeft);				// Rect, RectF, Circle
+	SIV3D_NAMED_PARAMETER(topRight);			// Rect, RectF, Circle
+	SIV3D_NAMED_PARAMETER(bottomLeft);			// Rect, RectF, Circle
+	SIV3D_NAMED_PARAMETER(bottomRight);			// Rect, RectF, Circle
+	SIV3D_NAMED_PARAMETER(topCenter);			// Rect, RectF, Circle
+	SIV3D_NAMED_PARAMETER(bottomCenter);		// Rect, RectF, Circle
+	SIV3D_NAMED_PARAMETER(leftCenter);			// Rect, RectF, Circle
+	SIV3D_NAMED_PARAMETER(rightCenter);			// Rect, RectF, Circle
+	SIV3D_NAMED_PARAMETER(top);					// Rect, RectF
+	SIV3D_NAMED_PARAMETER(bottom);				// Rect, RectF
+	SIV3D_NAMED_PARAMETER(left);				// Rect, RectF
+	SIV3D_NAMED_PARAMETER(right);				// Rect, RectF
 }
--- Siv3D/include/Siv3D/Rect.hpp
+++ Siv3D/include/Siv3D/Rect.hpp
@@ -12,6 +12,7 @@
 # pragma once
 # include "Common.hpp"
 # include "PointVector.hpp"
+# include "PredefinedNamedParameter.hpp"
 
 namespace s3d
 {
@@ -71,6 +72,310 @@ namespace s3d
 		};
 
 	SIV3D_DISABLE_MSVC_WARNINGS_POP()
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	(constructor)
+		//
+		////////////////////////////////////////////////////////////////
+
+		/// @brief デフォルトコンストラクタ
+		[[nodiscard]]
+		Rect() = default;
+
+		/// @brief 長方形を作成します。
+		/// @param _size 長方形の幅と高さ
+		[[nodiscard]]
+		explicit constexpr Rect(value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _size 長方形の幅と高さ
+		[[nodiscard]]
+		explicit constexpr Rect(Concept::Integral auto _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _w 長方形の幅
+		/// @param _h 長方形の高さ
+		[[nodiscard]]
+		constexpr Rect(value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _w 長方形の幅
+		/// @param _h 長方形の高さ
+		[[nodiscard]]
+		explicit constexpr Rect(Concept::Integral auto _w, Concept::Integral auto _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _size 長方形の大きさ
+		[[nodiscard]]
+		explicit constexpr Rect(size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _x 長方形の左上の点の X 座標
+		/// @param _y 長方形の左上の点の Y 座標
+		/// @param _size 長方形の縦横の大きさ
+		[[nodiscard]]
+		constexpr Rect(value_type _x, value_type _y, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _x 長方形の左上の点の X 座標
+		/// @param _y 長方形の左上の点の Y 座標
+		/// @param _size 長方形の縦横の大きさ
+		[[nodiscard]]
+		constexpr Rect(Concept::Integral auto _x, Concept::Integral auto _y, Concept::Integral auto _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _x 長方形の左上の点の X 座標
+		/// @param _y 長方形の左上の点の Y 座標
+		/// @param _w 長方形の幅
+		/// @param _h 	長方形の高さ
+		[[nodiscard]]
+		constexpr Rect(value_type _x, value_type _y, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _x 長方形の左上の点の X 座標
+		/// @param _y 長方形の左上の点の Y 座標
+		/// @param _w 長方形の幅
+		/// @param _h 	長方形の高さ
+		[[nodiscard]]
+		constexpr Rect(Concept::Integral auto _x, Concept::Integral auto _y, Concept::Integral auto _w, Concept::Integral auto _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _x 長方形の左上の点の X 座標
+		/// @param _y 長方形の左上の点の Y 座標
+		/// @param _size 長方形の大きさ
+		[[nodiscard]]
+		constexpr Rect(value_type _x, value_type _y, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _x 長方形の左上の点の X 座標
+		/// @param _y 長方形の左上の点の Y 座標
+		/// @param _size 長方形の大きさ
+		[[nodiscard]]
+		constexpr Rect(Concept::Integral auto _x, Concept::Integral auto _y, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _pos 長方形の左上の点の座標
+		/// @param _size 長方形の大きさ
+		[[nodiscard]]
+		constexpr Rect(position_type _pos, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _pos 長方形の左上の点の座標
+		/// @param _size 長方形の大きさ
+		[[nodiscard]]
+		constexpr Rect(position_type _pos, Concept::Integral auto _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _pos 長方形の左上の点の座標
+		/// @param _w 長方形の幅
+		/// @param _h 長方形の高さ
+		[[nodiscard]]
+		constexpr Rect(position_type _pos, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _pos 長方形の左上の点の座標
+		/// @param _w 長方形の幅
+		/// @param _h 長方形の高さ
+		[[nodiscard]]
+		constexpr Rect(position_type _pos, Concept::Integral auto _w, Concept::Integral auto _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _pos 長方形の左上の点の座標
+		/// @param _size 長方形の大きさ
+		[[nodiscard]]
+		constexpr Rect(position_type _pos, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _center 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::center_<position_type> _center, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _center 
+		/// @param _w 
+		/// @param _h 
+		[[nodiscard]]
+		constexpr Rect(Arg::center_<position_type> _center, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param _center 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::center_<position_type> _center, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param topLeft 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::topLeft_<position_type> topLeft, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param topLeft 
+		/// @param _w 
+		/// @param _h 
+		[[nodiscard]]
+		constexpr Rect(Arg::topLeft_<position_type> topLeft, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param topLeft 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::topLeft_<position_type> topLeft, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param topCenter 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::topCenter_<position_type> topCenter, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param topCenter 
+		/// @param _w 
+		/// @param _h 
+		[[nodiscard]]
+		constexpr Rect(Arg::topCenter_<position_type> topCenter, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param topCenter 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::topCenter_<position_type> topCenter, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param topRight 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::topRight_<position_type> topRight, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param topRight 
+		/// @param _w 
+		/// @param _h 
+		[[nodiscard]]
+		constexpr Rect(Arg::topRight_<position_type> topRight, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param topRight 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::topRight_<position_type> topRight, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param rightCenter 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::rightCenter_<position_type> rightCenter, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param rightCenter 
+		/// @param _w 
+		/// @param _h 
+		[[nodiscard]]
+		constexpr Rect(Arg::rightCenter_<position_type> rightCenter, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param rightCenter 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::rightCenter_<position_type> rightCenter, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param bottomRight 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::bottomRight_<position_type> bottomRight, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param bottomRight 
+		/// @param _w 
+		/// @param _h 
+		[[nodiscard]]
+		constexpr Rect(Arg::bottomRight_<position_type> bottomRight, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param bottomRight 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::bottomRight_<position_type> bottomRight, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param bottomCenter 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::bottomCenter_<position_type> bottomCenter, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param bottomCenter 
+		/// @param _w 
+		/// @param _h 
+		[[nodiscard]]
+		constexpr Rect(Arg::bottomCenter_<position_type> bottomCenter, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param bottomCenter 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::bottomCenter_<position_type> bottomCenter, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param bottomLeft 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::bottomLeft_<position_type> bottomLeft, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param bottomLeft 
+		/// @param _w 
+		/// @param _h 
+		[[nodiscard]]
+		constexpr Rect(Arg::bottomLeft_<position_type> bottomLeft, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param bottomLeft 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::bottomLeft_<position_type> bottomLeft, size_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param leftCenter 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::leftCenter_<position_type> leftCenter, value_type _size) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param leftCenter 
+		/// @param _w 
+		/// @param _h 
+		[[nodiscard]]
+		constexpr Rect(Arg::leftCenter_<position_type> leftCenter, value_type _w, value_type _h) noexcept;
+
+		/// @brief 長方形を作成します。
+		/// @param leftCenter 
+		/// @param _size 
+		[[nodiscard]]
+		constexpr Rect(Arg::leftCenter_<position_type> leftCenter, size_type _size) noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator ==
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		friend constexpr bool operator ==(const Rect& lhs, const Rect& rhs) noexcept
+		{
+			return ((lhs.pos == rhs.pos)
+				&& (lhs.size == rhs.size));
+		}
+
+
+
+
+
+
+
 
 
 
