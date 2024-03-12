8b45114dcc453bbf2a092053da61393a00600465
Ryo Suzuki <reputeless+github@gmail.com>
2024-03-12 14:17:47 +0900

    Update Window.hpp

--- Siv3D/include/Siv3D/Window.hpp
+++ Siv3D/include/Siv3D/Window.hpp
@@ -12,6 +12,7 @@
 # pragma once
 # include "Common.hpp"
 # include "StringView.hpp"
+# include "Format.hpp"
 
 namespace s3d
 {
@@ -19,5 +20,19 @@ namespace s3d
 	{
 		/// @brief ウィンドウのデフォルトのタイトル
 		inline constexpr StringView DefaultTitle = U"Siv3D App";
+	
+		/// @brief ウィンドウのタイトルを変更します。
+		/// @param title 新しいタイトル
+		void SetTitle(const String& title);
+
+		/// @brief ウィンドウのタイトルを変更します。
+		/// @tparam ...Args 新しいタイトルの値の型
+		/// @param ...args 新しいタイトル
+		void SetTitle(const Concept::Formattable auto&... args);
+
+		/// @brief 現在のウィンドウのタイトルを返します。
+		/// @return  現在のウィンドウのタイトル
+		[[nodiscard]]
+		const String& GetTitle() noexcept;
 	}
 }
