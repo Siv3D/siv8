5757e5e74f3d661e4e508a2eb531d3087ac0a209
Ryo Suzuki <reputeless+github@gmail.com>
2024-04-26 01:14:15 +0900

    update

--- Siv3D/src/Siv3D/ImageDecoder/SivImageDecoder.cpp
+++ Siv3D/src/Siv3D/ImageDecoder/SivImageDecoder.cpp
@@ -18,6 +18,12 @@ namespace s3d
 {
 	namespace ImageDecoder
 	{
+		////////////////////////////////////////////////////////////////
+		//
+		//	GetImageInfo
+		//
+		////////////////////////////////////////////////////////////////
+
 		Optional<ImageInfo> GetImageInfo(const FilePathView path, const ImageFormat imageFormat)
 		{
 			BinaryReader reader{ path };
@@ -35,6 +41,12 @@ namespace s3d
 			return SIV3D_ENGINE(ImageDecoder)->getImageInfo(reader, {}, imageFormat);
 		}
 
+		////////////////////////////////////////////////////////////////
+		//
+		//	Decode
+		//
+		////////////////////////////////////////////////////////////////
+
 		Image Decode(const FilePathView path, const PremultiplyAlpha premultiplyAlpha, const ImageFormat imageFormat)
 		{
 			BinaryReader reader{ path };
@@ -51,6 +63,12 @@ namespace s3d
 		{
 			return SIV3D_ENGINE(ImageDecoder)->decode(reader, {}, premultiplyAlpha, imageFormat);
 		}
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	DecodeGray16
+		//
+		////////////////////////////////////////////////////////////////
 
 		Grid<uint16> DecodeGray16(const FilePathView path, const ImageFormat imageFormat)
 		{
@@ -68,16 +86,34 @@ namespace s3d
 		{
 			return SIV3D_ENGINE(ImageDecoder)->decodeGray16(reader, {}, imageFormat);
 		}
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	Add
+		//
+		////////////////////////////////////////////////////////////////
 
 		bool Add(std::unique_ptr<IImageDecoder>&& decoder)
 		{
 			return SIV3D_ENGINE(ImageDecoder)->add(std::move(decoder));
 		}
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	Remove
+		//
+		////////////////////////////////////////////////////////////////
 
 		void Remove(const StringView decoderName)
 		{
 			return SIV3D_ENGINE(ImageDecoder)->remove(decoderName);
 		}
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	IsRegistered
+		//
+		////////////////////////////////////////////////////////////////
 
 		bool IsRegistered(const StringView decoderName) noexcept
 		{
@@ -91,6 +127,12 @@ namespace s3d
 
 			return false;
 		}
+		
+		////////////////////////////////////////////////////////////////
+		//
+		//	Enum
+		//
+		////////////////////////////////////////////////////////////////
 
 		const Array<std::unique_ptr<IImageDecoder>>& Enum() noexcept
 		{
