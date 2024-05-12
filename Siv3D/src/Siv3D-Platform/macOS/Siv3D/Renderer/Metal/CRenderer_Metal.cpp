bb7f6391792b82c4e07775079ccc2046e4322bbe
Ryo Suzuki <reputeless+github@gmail.com>
2024-05-12 15:44:49 +0900

    Metal 準備

---
+++ Siv3D/src/Siv3D-Platform/macOS/Siv3D/Renderer/Metal/CRenderer_Metal.cpp
@@ -0,0 +1,73 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2024 Ryo Suzuki
+//	Copyright (c) 2016-2024 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include "CRenderer_Metal.hpp"
+# include <Siv3D/WindowState.hpp>
+# include <Siv3D/Window/IWindow.hpp>
+# include <Siv3D/Engine/Siv3DEngine.hpp>
+# include <Siv3D/EngineLog.hpp>
+
+namespace s3d
+{
+	CRenderer_Metal::~CRenderer_Metal()
+	{
+		LOG_SCOPED_DEBUG("CRenderer_Metal::~CRenderer_Metal()");
+	}
+
+	void CRenderer_Metal::init()
+	{
+		LOG_SCOPED_DEBUG("CRenderer_Metal::init()");
+
+
+
+	}
+
+	void CRenderer_Metal::clear()
+	{
+
+	}
+
+	void CRenderer_Metal::flush()
+	{
+
+	}
+
+	bool CRenderer_Metal::present()
+	{
+		return(true);
+	}
+
+	SceneStyle& CRenderer_Metal::getSceneStyle() noexcept
+	{
+
+	}
+
+	void CRenderer_Metal::setSceneResizeMode(const ResizeMode resizeMode)
+	{
+
+	}
+
+	ResizeMode CRenderer_Metal::getSceneResizeMode() const noexcept
+	{
+
+	}
+
+	void CRenderer_Metal::resizeSceneBuffer(const Size size)
+	{
+
+	}
+
+	const Size& CRenderer_Metal::getSceneBufferSize() const noexcept
+	{
+
+	}
+}
+ 
---
+++ Siv3D/src/Siv3D-Platform/macOS/Siv3D/Renderer/Metal/CRenderer_Metal.hpp
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2024 Ryo Suzuki
+//	Copyright (c) 2016-2024 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include <Siv3D/Renderer/IRenderer.hpp>
+
+namespace s3d
+{
+	class CRenderer_Metal final : public ISiv3DRenderer
+	{
+	public:
+
+		~CRenderer_Metal() override;
+
+		void init() override;
+
+		void clear() override;
+
+		void flush() override;
+
+		bool present() override;
+
+		SceneStyle& getSceneStyle() noexcept override;
+
+		void setSceneResizeMode(ResizeMode resizeMode) override;
+
+		ResizeMode getSceneResizeMode() const noexcept override;
+
+		void resizeSceneBuffer(Size size) override;
+
+		const Size& getSceneBufferSize() const noexcept override;
+
+	private:
+
+	};
+}
