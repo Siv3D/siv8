400c775f3c18cc3608d08c00ccb30a5d7a2f42f2
KakeyamaY <ykakeyama3014@gmail.com>
2024-07-06 23:03:14 +0900

    fix typo in SivMSL.cpp (#36)

--- Siv3D/src/Siv3D/MSL/SivMSL.cpp
+++ Siv3D/src/Siv3D/MSL/SivMSL.cpp
@@ -10,7 +10,7 @@
 //-----------------------------------------------
 
 # include <Siv3D/MSL.hpp>
-# include <Siv3D/VertexSHader.hpp>
+# include <Siv3D/VertexShader.hpp>
 # include <Siv3D/PixelShader.hpp>
 
 namespace s3d
