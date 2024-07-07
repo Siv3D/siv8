a57dfedcaed2ef06f0e33f2e2b52dfa40b827e59
Ryo Suzuki <reputeless+github@gmail.com>
2024-07-07 18:26:22 +0900

    Update Circle.hpp

--- Siv3D/include/Siv3D/Circle.hpp
+++ Siv3D/include/Siv3D/Circle.hpp
@@ -137,7 +137,363 @@ namespace s3d
 		[[nodiscard]]
 		constexpr Circle(Arg::leftCenter_<position_type> _leftCenter, Concept::Arithmetic auto _r) noexcept;
 
+		/// @brief 指定した 2 点を通る円を作成します。
+		/// @param p0 点
+		/// @param p1 点
+		[[nodiscard]]
+		Circle(position_type p0, position_type p1) noexcept;
 
+		/// @brief 指定した 3 点を通る円を作成します。
+		/// @param p0 点
+		/// @param p1 点
+		/// @param p2 点
+		[[nodiscard]]
+		Circle(const position_type& p0, const position_type& p1, const position_type& p2) noexcept;
+
+		/// @brief 指定した直径を持つ円を作成します。
+		/// @param diameter 直径
+		[[nodiscard]]
+		explicit Circle(const Line& diameter) noexcept;
+	
+		/// @brief 中心と、円周上の点を指定して円を作成します。
+		/// @param _center 中心座標
+		/// @param p 円周上の点
+		[[nodiscard]]
+		Circle(Arg::center_<position_type> _center, const position_type& p) noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	operator ==
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		friend constexpr bool operator ==(const Circle& lhs, const Circle& rhs) noexcept
+		{
+			return ((lhs.center == rhs.center)
+				 && (lhs.r == rhs.r));
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	withX
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		constexpr Circle withX(value_type _x) const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	withY
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		constexpr Circle withY(value_type _y) const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	withCenter
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		constexpr Circle withCenter(value_type _x, value_type _y) const noexcept;
+
+		[[nodiscard]]
+		constexpr Circle withCenter(position_type _center) const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	withR
+		//
+		////////////////////////////////////////////////////////////////
+
+		[[nodiscard]]
+		constexpr Circle withR(value_type _r) const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	setPos
+		//
+		////////////////////////////////////////////////////////////////
+
+		constexpr Circle& setPos(value_type _x, value_type _y) noexcept;
+
+		constexpr Circle& setPos(position_type _center) noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	setCenter
+		//
+		////////////////////////////////////////////////////////////////
+
+		constexpr Circle& setCenter(value_type _x, value_type _y) noexcept;
+
+		constexpr Circle& setCenter(position_type _center) noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	setR
+		//
+		////////////////////////////////////////////////////////////////
+
+		constexpr Circle& setR(value_type _r) noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	set
+		//
+		////////////////////////////////////////////////////////////////
+
+		constexpr Circle& set(value_type _x, value_type _y, size_type _r) noexcept;
+
+		constexpr Circle& set(position_type _center, size_type _r) noexcept;
+
+		constexpr Circle& set(Arg::center_<position_type> _center, size_type _r) noexcept;
+
+		constexpr Circle& set(Arg::topLeft_<position_type> topLeft, size_type _r) noexcept;
+
+		constexpr Circle& set(Arg::topCenter_<position_type> topCenter, size_type _r) noexcept;
+
+		constexpr Circle& set(Arg::topRight_<position_type> topRight, size_type _r) noexcept;
+
+		constexpr Circle& set(Arg::rightCenter_<position_type> rightCenter, size_type _r) noexcept;
+
+		constexpr Circle& set(Arg::bottomRight_<position_type> bottomRight, size_type _r) noexcept;
+
+		constexpr Circle& set(Arg::bottomCenter_<position_type> bottomCenter, size_type _r) noexcept;
+
+		constexpr Circle& set(Arg::bottomLeft_<position_type> bottomLeft, size_type _r) noexcept;
+
+		constexpr Circle& set(Arg::leftCenter_<position_type> leftCenter, size_type _r) noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	movedBy
+		//
+		////////////////////////////////////////////////////////////////
+
+		/// @brief 中心座標を移動した新しい円を返します。
+		/// @param _x X 軸方向の移動量
+		/// @param _y Y 軸方向の移動量
+		/// @return 新しい円
+		[[nodiscard]]
+		constexpr Circle movedBy(value_type _x, value_type _y) const noexcept;
+
+		/// @brief 中心座標を移動した新しい円を返します。
+		/// @param v 移動量
+		/// @return 新しい円
+		[[nodiscard]]
+		constexpr Circle movedBy(position_type v) const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	moveBy
+		//
+		////////////////////////////////////////////////////////////////
+
+		/// @brief 中心座標を移動させます。
+		/// @param _x X 軸方向の移動量
+		/// @param _y Y 軸方向の移動量
+		/// @return *this
+		constexpr Circle& moveBy(value_type _x, value_type _y) noexcept;
+
+		/// @brief 中心座標を移動させます。
+		/// @param v 移動量
+		/// @return *this
+		constexpr Circle& moveBy(position_type v) noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	stretched
+		//
+		////////////////////////////////////////////////////////////////
+
+		/// @brief 半径を変化させた新しい円を返します。
+		/// @param size 半径の変化量
+		/// @return 新しい円
+		[[nodiscard]]
+		constexpr Circle stretched(value_type size) const noexcept;
+
+		/// @brief X 軸、Y 軸上で径を変化させて楕円を作成します。
+		/// @param _x X 軸上の径の変化量
+		/// @param _y Y 軸上の径の変化量
+		/// @return 楕円
+		[[nodiscard]]
+		constexpr Ellipse stretched(double _x, double _y) const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	scaled
+		//
+		////////////////////////////////////////////////////////////////
+
+		/// @brief 半径を拡大した新しい円を返します。
+		/// @param s 拡大倍率
+		/// @return 新しい円
+		[[nodiscard]]
+		constexpr Circle scaled(double s) const noexcept;
+
+		/// @brief X 軸、Y 軸上に拡大して楕円を作成します。
+		/// @param sx X 軸方向の拡大倍率
+		/// @param sy Y 軸方向の拡大倍率
+		/// @return 楕円
+		[[nodiscard]]
+		constexpr Ellipse scaled(double sx, double sy) const noexcept;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	top, right, bottom, left
+		//
+		////////////////////////////////////////////////////////////////
+
+		/// @brief 円周上で最も上にある点の座標を返します。
+		/// @return 最も上にある点の座標
+		[[nodiscard]]
+		constexpr position_type top() const noexcept;
+
+		/// @brief 円周上で最も右にある点の座標を返します。
+		/// @return 最も右にある点の座標
+		[[nodiscard]]
+		constexpr position_type right() const noexcept;
+
+		/// @brief 円周上で最も下にある点の座標を返します。
+		/// @return 最も下にある点の座標
+		[[nodiscard]]
+		constexpr position_type bottom() const noexcept;
+
+		/// @brief 円周上で最も左にある点の座標を返します。
+		/// @return 最も左にある点の座標
+		[[nodiscard]]
+		constexpr position_type left() const noexcept;
+
+
+
+
+		/// @brief X 軸に平行な直径（線分）を返します。
+		/// @return X 軸に平行な直径（線分）
+		[[nodiscard]]
+		constexpr Line horizontalDiameter() const noexcept;
+
+		/// @brief Y 軸に平行な直径（線分）を返します。
+		/// @return Y 軸に平行な直径（線分）
+		[[nodiscard]]
+		constexpr Line verticalDiameter() const noexcept;
+
+		/// @brief 円の面積を返します。
+		/// @return 円の面積
+		[[nodiscard]]
+		constexpr value_type area() const noexcept;
+
+		/// @brief 円の周の長さを返します。
+		/// @return 円の周の長さ
+		[[nodiscard]]
+		constexpr value_type perimeter() const noexcept;
+
+		/// @brief 円に外接する正方形を返します。
+		/// @return 円に外接する正方形
+		[[nodiscard]]
+		constexpr RectF boundingRect() const noexcept;
+
+		/// @brief 円周上の点をラジアン (12 時方向が 0, 3 時方向が π/2, 6 時方向が π) で取得します。
+		/// @param angle 方向（ラジアン）
+		/// @return 円周上の点
+		[[nodiscard]]
+		position_type getPointByAngle(double angle) const noexcept;
+
+		/// @brief 円から Polygon を作成します。
+		/// @param quality 品質
+		/// @return Polygon
+		[[nodiscard]]
+		Polygon asPolygon(uint32 quality = 24) const;
+
+		[[nodiscard]]
+		Polygon pieAsPolygon(double startAngle, double angle, uint32 quality = 24) const;
+
+		[[nodiscard]]
+		Polygon arcAsPolygon(double startAngle, double angle, double innerThickness, double outerThickness, uint32 quality = 24) const;
+
+		[[nodiscard]]
+		constexpr Circle lerp(const Circle& other, double f) const noexcept;
+
+		[[nodiscard]]
+		uint64 hash() const noexcept;
+
+		template <class Shape2DType>
+		[[nodiscard]]
+		constexpr bool intersects(const Shape2DType& other) const;
+
+		//template <class Shape2DType>
+		//[[nodiscard]]
+		//Optional<Array<Vec2>> intersectsAt(const Shape2DType& other) const;
+
+		//template <class Shape2DType>
+		//[[nodiscard]]
+		//bool contains(const Shape2DType& other) const;
+
+		/// @brief 円が現在のフレームで左クリックされ始めたかを返します。
+		/// @return 円が現在のフレームで左クリックされ始めた場合 true, それ以外の場合は false
+		[[nodiscard]]
+		bool leftClicked() const noexcept;
+
+		/// @brief 円が左クリックされているかを返します。
+		/// @return 円が左クリックされている場合 true, それ以外の場合は false
+		[[nodiscard]]
+		bool leftPressed() const noexcept;
+
+		/// @brief 現在のフレームで円への左クリックが離されたかを返します。
+		/// @return 現在のフレームで円への左クリックが離された場合 true, それ以外の場合は false
+		[[nodiscard]]
+		bool leftReleased() const noexcept;
+
+		/// @brief 円が現在のフレームで右クリックされ始めたかを返します。
+		/// @return 円が現在のフレームで右クリックされ始めた場合 true, それ以外の場合は false
+		[[nodiscard]]
+		bool rightClicked() const noexcept;
+
+		/// @brief 円が右クリックされているかを返します。
+		/// @return 円が右クリックされている場合 true, それ以外の場合は false
+		[[nodiscard]]
+		bool rightPressed() const noexcept;
+
+		/// @brief 現在のフレームで円への右クリックが離されたかを返します。
+		/// @return 現在のフレームで円への右クリックが離された場合 true, それ以外の場合は false
+		[[nodiscard]]
+		bool rightReleased() const noexcept;
+
+		/// @brief 円上にマウスカーソルがあるかを返します。
+		/// @return 円上にマウスカーソルがある場合 true, それ以外の場合は false
+		[[nodiscard]]
+		bool mouseOver() const noexcept;
+
+
+
+
+
+
+		//const Circle& paint(Image& dst, const Color& color, Antialiased antialiased = Antialiased::Yes) const;
+
+		//const Circle& overwrite(Image& dst, const Color& color, Antialiased antialiased = Antialiased::Yes) const;
+
+		//const Circle& paintFrame(Image& dst, double innerThickness, double outerThickness, const Color& color, Antialiased antialiased = Antialiased::Yes) const;
+
+		//const Circle& overwriteFrame(Image& dst, double innerThickness, double outerThickness, const Color& color, Antialiased antialiased = Antialiased::Yes) const;
+
+		//const Circle& paintPie(Image& dst, double startAngle, double angle, const Color& color) const;
+
+		//const Circle& overwritePie(Image& dst, double startAngle, double angle, const Color& color) const;
+
+		//const Circle& paintArc(Image& dst, double startAngle, double angle, double innerThickness, double outerThickness, const Color& color) const;
+
+		//const Circle& overwriteArc(Image& dst, double startAngle, double angle, double innerThickness, double outerThickness, const Color& color) const;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	draw
+		//
+		////////////////////////////////////////////////////////////////
 
 		/// @brief 円を描きます。
 		/// @param color 円の色
@@ -149,6 +505,12 @@ namespace s3d
 		/// @param outerColor 円の外側の色
 		/// @return *this
 		const Circle& draw(const ColorF& innerColor, const ColorF& outerColor) const;
+
+		////////////////////////////////////////////////////////////////
+		//
+		//	drawFrame
+		//
+		////////////////////////////////////////////////////////////////
 
 		/// @brief 円の枠を描きます
 		/// @param thickness 枠の太さ
