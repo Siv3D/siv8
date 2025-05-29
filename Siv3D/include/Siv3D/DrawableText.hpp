ffe37d1f70a8ed199896ee09d36250ed03f2f0b9
Ryo Suzuki <reputeless+github@gmail.com>
2025-05-29 23:48:06 +0900

    DrawableText 準備

---
+++ Siv3D/include/Siv3D/DrawableText.hpp
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2025 Ryo Suzuki
+//	Copyright (c) 2016-2025 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include "Common.hpp"
+# include "String.hpp"
+# include "Font.hpp"
+# include "Format.hpp"
+
+namespace s3d
+{
+	struct DrawableText
+	{
+		Font font;
+
+		String text;
+
+		Array<ResolvedGlyph> resolvedGlyphs;
+
+		[[nodiscard]]
+		DrawableText() = default;
+
+		[[nodiscard]]
+		DrawableText(const Font& _font, const String& _text);
+
+		[[nodiscard]]
+		DrawableText(const Font& _font, String&& _text);
+
+		[[nodiscard]]
+		Array<double> getXAdvances() const;
+
+		[[nodiscard]]
+		Array<double> getXAdvances(double size) const;
+	};
+}
---
+++ Siv3D/src/Siv3D/DrawableText/SivDrawableText.cpp
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2025 Ryo Suzuki
+//	Copyright (c) 2016-2025 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/DrawableText.hpp>
+
+namespace s3d
+{
+	DrawableText::DrawableText(const Font& _font, const String& _text)
+		: DrawableText{ _font, String{ _text } } {}
+
+	DrawableText::DrawableText(const Font& _font, String&& _text)
+		: font{ _font }
+		, text{ std::move(_text) }
+		, resolvedGlyphs{ font.getResolvedGlyphs(text) } {}
+	
+	Array<double> DrawableText::getXAdvances() const
+	{
+		return getXAdvances(font.baseSize());
+	}
+
+	Array<double> DrawableText::getXAdvances(const double textSize) const
+	{
+		return{};
+		//return SIV3D_ENGINE(Font)->getXAdvances(font.id(), text, clusters, size);
+	}
+}
