24a224e18696bee3afc9956558e98a2f1ce86ef8
Ryo Suzuki <reputeless+github@gmail.com>
2025-10-06 23:07:39 +0900

    Update SivSuperEllipse.cpp

--- Siv3D/src/Siv3D/SuperEllipse/SivSuperEllipse.cpp
+++ Siv3D/src/Siv3D/SuperEllipse/SivSuperEllipse.cpp
@@ -34,4 +34,44 @@ namespace s3d
 		const long double lnA = (std::logl(4.0L * _a * _b) + 2.0L * std::lgammal(x1) - std::lgammal(x2));
 		return static_cast<double>(std::expl(lnA));
 	}
+
+	////////////////////////////////////////////////////////////////
+	//
+	//	getPointByAngle
+	//
+	////////////////////////////////////////////////////////////////
+
+	SuperEllipse::position_type SuperEllipse::getPointByAngle(const double angle) const noexcept
+	{
+		if (n <= 0.0)
+		{
+			return center;
+		}
+
+		const long double ax = Abs(axes.x);
+		const long double ay = Abs(axes.y);
+
+		if ((ax == 0.0L) || (ay == 0.0L))
+		{
+			return { center.x, center.y };
+		}
+
+		const long double s = std::sin(angle);
+		const long double c = std::cos(angle);
+		const long double p = (2.0L / static_cast<long double>(n));
+
+		auto SignedPow = [p](long double v) -> long double
+		{
+			if (v == 0.0L)
+			{
+				return 0.0L;
+			}
+
+			return std::copysignl(std::powl(Abs(v), p), v);
+		};
+
+		const long double xs = SignedPow(s);
+		const long double yc = -SignedPow(c);
+		return{ (center.x + ax * xs), (center.y + ay * yc) };
+	}
 }
