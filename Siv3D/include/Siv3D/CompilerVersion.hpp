358512ebdb8e1fd3b8b82af5b546f7b47395d78a
Ryo Suzuki <reputeless+github@gmail.com>
2025-12-16 11:50:40 +0900

    VS2026

--- Siv3D/include/Siv3D/CompilerVersion.hpp
+++ Siv3D/include/Siv3D/CompilerVersion.hpp
@@ -13,18 +13,18 @@
 # include <version>
 # include "Platform.hpp"
 
-// Visual Studio 2022 バージョン要件チェック
-// Visual Studio 2022 version requirement check
-# if (SIV3D_COMPILER(MSVC) && (_MSC_VER < 1944))
+// Visual Studio 2026 バージョン要件チェック
+// Visual Studio 2026 version requirement check
+# if (SIV3D_COMPILER(MSVC) && (_MSC_VER < 1950))
 
-	# pragma message("Siv3D のビルドには Visual Studio 2022 version 17.14 以降が必要です。Visual Studio をアップデートするか、最新版をダウンロードしてください。")
-	# error "Siv3D requires Visual Studio 2022 version 17.14 or later. Please update your Visual Studio installation or download the latest version."
+	# pragma message("Siv3D のビルドには Visual Studio 2026 18.0 が必要です。")
+	# error "Siv3D requires Visual Studio 2026 version 18.0 or later."
 
 # endif
 
 // C++ 標準ライブラリのバージョン要件チェック
 // C++ Standard Library version requirement check
-# if (SIV3D_COMPILER(MSVC) && (_MSVC_STL_UPDATE < 202501L))
+# if (SIV3D_COMPILER(MSVC) && (_MSVC_STL_UPDATE < 202508L))
 
 	# pragma message("Siv3D のビルドには最新の C++ 標準ライブラリが必要です。Visual Studio Installer を開き、「C++ によるデスクトップ開発」ワークロードから最新の C++ x64/x86 ビルドツールをインストールしてください。")
 	# error "Siv3D requires the latest C++ Standard Library. Please open Visual Studio Installer, select the 'Desktop development with C++' workload, and install the latest C++ x64/x86 build tools."
