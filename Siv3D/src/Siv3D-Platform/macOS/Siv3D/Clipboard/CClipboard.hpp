1dd05be2ab9f2b73e3ee6c47acd7ef4419e0afc7
Ryo Suzuki <reputeless+github@gmail.com>
2026-02-18 16:29:03 +0900

    update

--- Siv3D/src/Siv3D-Platform/macOS/Siv3D/Clipboard/CClipboard.hpp
+++ Siv3D/src/Siv3D-Platform/macOS/Siv3D/Clipboard/CClipboard.hpp
@@ -24,6 +24,40 @@ namespace s3d
 
 		void init() override;
 
+		bool hasChanged() override;
+
+		uint64 getSequenceNumber() override;
+
+		void clear() override;
+
+		void setText(StringView text) override;
+
+		bool getText(String& text) override;
+
+		bool hasText() override;
+
+		void setImage(const Image& image) override;
+
+		bool getImage(Image& image, PremultiplyAlpha premultiplyAlpha) override;
+
+		bool hasImage() override;
+
+		void setFilePaths(const Array<FilePath>& paths) override;
+
+		bool getFilePaths(Array<FilePath>& paths) override;
+
+		bool hasFilePaths() override;
+
+		void setRichText(StringView richText, StringView plainTextFallback) override;
+
+		void setHTML(StringView html, StringView plainTextFallback) override;
+
+		void setData(StringView mimeType, const void* data, size_t size) override;
+
+		bool getData(StringView mimeType, Blob& data) override;
+
+		Array<String> getAvailableMimeTypes() override;
+
 	private:
 
 	};
