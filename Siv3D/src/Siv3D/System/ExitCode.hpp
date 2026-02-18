f7de93b125b55417975c72e085fc1c1eae8f5fd3
Ryo Suzuki <reputeless+github@gmail.com>
2026-02-18 17:04:34 +0900

    exit code

---
+++ Siv3D/src/Siv3D/System/ExitCode.hpp
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2026 Ryo Suzuki
+//	Copyright (c) 2016-2026 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include <Siv3D/Common.hpp>
+
+namespace s3d
+{
+	void SetExitCode(const int32 exitCode) noexcept;
+
+	[[nodiscard]]
+	int32 GetExitCode() noexcept;
+}
