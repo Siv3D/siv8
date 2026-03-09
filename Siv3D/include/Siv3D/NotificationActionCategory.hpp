2cec21f8e661463654a25a7af7362d8d1e3507cd
Ryo Suzuki <reputeless+github@gmail.com>
2026-03-09 19:05:49 +0900

    Update NotificationActionCategory.hpp

--- Siv3D/include/Siv3D/NotificationActionCategory.hpp
+++ Siv3D/include/Siv3D/NotificationActionCategory.hpp
@@ -16,18 +16,50 @@ namespace s3d
 {
 	struct FormatData;
 
+	/// @brief 通知に設定する定義済みアクションカテゴリ
 	enum class NotificationActionCategory : uint8
 	{
+		/// @brief アクション無し
 		Default = 0,
+		
+		/// @brief 「Open」
+		/// @remark actionID は U"open" です
 		Open,
+		
+		/// @brief 「Open」「Dismiss」
+		/// @remark actionID はそれぞれ U"open", U"dismiss" です
 		OpenDismiss,
+		
+		/// @brief 「OK」
+		/// @remark actionID は U"ok" です
 		OK,
+		
+		/// @brief 「OK」「Cancel」
+		/// @remark actionID はそれぞれ U"ok", U"cancel" です
 		OKCancel,
+		
+		/// @brief 「Yes」「No」
+		/// @remark actionID はそれぞれ U"yes", U"no" です
 		YesNo,
+		
+		/// @brief 「Yes」「No」「Cancel」
+		/// @remark actionID はそれぞれ U"yes", U"no", U"cancel" です
 		YesNoCancel,
+		
+		/// @brief 「Accept」「Decline」
+		/// @remark actionID はそれぞれ U"accept", U"decline" です
 		AcceptDecline,
+		
+		/// @brief 「Accept」「Decline」「Later」
+		/// @remark actionID はそれぞれ U"accept", U"decline", U"later" です
 		AcceptDeclineLater,
+		
+		/// @brief 「Retry」「Cancel」
+		/// @remark actionID はそれぞれ U"retry", U"cancel" です
 		RetryCancel,
+		
+		/// @brief 「View」「Dismiss」
+		/// @remark actionID はそれぞれ U"view", U"dismiss" です
 		ViewDismiss,
 	};
 
