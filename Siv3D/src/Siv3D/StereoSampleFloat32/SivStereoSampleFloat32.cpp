5e3b39463ccad7ce4eb5480d873205a7b5877e5f
Ryo Suzuki <reputeless+github@gmail.com>
2026-03-10 19:16:24 +0900

    update

---
+++ Siv3D/src/Siv3D/StereoSampleFloat32/SivStereoSampleFloat32.cpp
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2026 Ryo Suzuki
+//	Copyright (c) 2016-2026 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/WaveSample.hpp>
+# include <Siv3D/FormatData.hpp>
+# include <Siv3D/FloatFormatter.hpp>
+
+namespace s3d
+{		
+	////////////////////////////////////////////////////////////////
+	//
+	//	Formatter
+	//
+	////////////////////////////////////////////////////////////////
+
+	void Formatter(FormatData& formatData, const StereoSampleFloat32 value)
+	{
+		formatData.string.push_back(U'(');
+		detail::AppendFloat(formatData.string, value.left);
+		formatData.string.append(U", "_sv);
+		detail::AppendFloat(formatData.string, value.right);
+		formatData.string.push_back(U')');
+	}
+}
---
+++ Siv3D/src/Siv3D/StereoSampleInt16/SivStereoSampleInt16.cpp
@@ -0,0 +1,39 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2026 Ryo Suzuki
+//	Copyright (c) 2016-2026 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/WaveSample.hpp>
+# include <Siv3D/FormatData.hpp>
+# include <Siv3D/IntFormatter.hpp>
+
+namespace s3d
+{		
+	////////////////////////////////////////////////////////////////
+	//
+	//	Formatter
+	//
+	////////////////////////////////////////////////////////////////
+
+	void Formatter(FormatData& formatData, const StereoSampleInt16 value)
+	{
+		constexpr size_t BufferSize = ((detail::Int32Width * 2) + 4 + 1);
+		char32 buffer[BufferSize];
+		char32* p = buffer;
+
+		*(p++) = U'(';
+		detail::AppendInt32(&p, value.left);
+		*(p++) = U',';
+		*(p++) = U' ';
+		detail::AppendInt32(&p, value.right);
+		*(p++) = U')';
+
+		formatData.string.append(buffer, (p - buffer));
+	}
+}
