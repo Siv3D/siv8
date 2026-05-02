906914afbc7c937924f35c9e0e95661b94494aa3
Ryo Suzuki <3108136+Reputeless@users.noreply.github.com>
2026-05-02 09:23:31 +0900

    Update SivSystem_macOS.mm

--- Siv3D/src/Siv3D-Platform/macOS/Siv3D/System/SivSystem_macOS.mm
+++ Siv3D/src/Siv3D-Platform/macOS/Siv3D/System/SivSystem_macOS.mm
@@ -17,35 +17,21 @@
 
 namespace s3d
 {
-	namespace detail
+	namespace
 	{
-		[[nodiscard]]
-		static bool MacOS_OpenURLInBrowser(const char* _url)
+		static NSString* ToNSString(const StringView s)
 		{
-			@autoreleasepool
-			{
-				NSString* url = [NSString stringWithUTF8String:_url];
-				NSURL* ns_url = [NSURL URLWithString:url];
-				const bool result = [[NSWorkspace sharedWorkspace]
-									openURLs:@[ns_url]
-									withAppBundleIdentifier:nil
-									options:NSWorkspaceLaunchDefault
-									additionalEventParamDescriptor:nil
-									launchIdentifiers:nil];
-				return result;
-			}
+			const std::string utf8 = Unicode::ToUTF8(s);
+			return [[[NSString alloc] initWithBytes:utf8.data()
+											 length:utf8.size()
+										   encoding:NSUTF8StringEncoding] autorelease];
 		}
-	
-		[[nodiscard]]
-		static bool MacOS_OpenHTMLInBrowser(const char* _path)
+
+		static NSURL* ToFileURL(const FilePathView path, const bool isDirectory = false)
 		{
-			@autoreleasepool
-			{
-				NSString* pathString = [NSString stringWithUTF8String:_path];
-				NSURL* url = [NSURL fileURLWithPath:pathString];
-				const bool result = [[NSWorkspace sharedWorkspace] openURL:url];
-				return result;
-			}
+			const FilePath fullPath = FileSystem::FullPath(path);
+			NSString* nsPath = ToNSString(fullPath);
+			return [NSURL fileURLWithPath:nsPath isDirectory:isDirectory];
 		}
 	}
 
@@ -53,22 +39,131 @@ namespace s3d
 	{
 		bool OpenInBrowser(const URLView url)
 		{
-			if (const bool isWebPage = (url.starts_with(U"http://") || url.starts_with(U"https://"))) // Web ページ
+			@autoreleasepool
 			{
-                return detail::MacOS_OpenURLInBrowser(Unicode::ToUTF8(url).c_str());
-			}
-			else // ローカルファイル
-			{
-				const String extension = FileSystem::Extension(url);
-				
-				const bool isHTML = ((extension == U"html") || (extension == U"htm"));
+				if (url.starts_with(U"http://") || url.starts_with(U"https://"))
+				{
+					NSString* nsURLString = ToNSString(url);
+					NSURL* nsURL = [NSURL URLWithString:nsURLString];
 
-				if (not isHTML)
+					if (nsURL == nil)
+					{
+						return false;
+					}
+
+					return ([[NSWorkspace sharedWorkspace] openURL:nsURL] == YES);
+				}
+				else
+				{
+					const String extension = FileSystem::Extension(url);
+
+					const bool isHTML = ((extension == U"html") || (extension == U"htm"));
+
+					if (not isHTML)
+					{
+						return false;
+					}
+
+					NSURL* fileURL = ToFileURL(url, false);
+
+					return ([[NSWorkspace sharedWorkspace] openURL:fileURL] == YES);
+				}
+			}
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//  ShowInFileManager
+		//
+		////////////////////////////////////////////////////////////////
+
+		bool ShowInFileManager(const FilePathView path)
+		{
+			@autoreleasepool
+			{
+				const FilePath fullPath = FileSystem::FullPath(path);
+
+				if (FileSystem::IsFile(fullPath))
+				{
+					NSURL* fileURL = ToFileURL(fullPath, false);
+
+					// activateFileViewerSelectingURLs: は void なので、
+					// 存在確認済みなら true とする
+					[[NSWorkspace sharedWorkspace] activateFileViewerSelectingURLs:@[ fileURL ]];
+
+					return true;
+				}
+				else if (FileSystem::IsDirectory(fullPath))
+				{
+					NSURL* dirURL = ToFileURL(fullPath, true);
+
+					return ([[NSWorkspace sharedWorkspace] openURL:dirURL] == YES);
+				}
+
+				return false;
+			}
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//  LaunchFile
+		//
+		////////////////////////////////////////////////////////////////
+
+		bool LaunchFile(const FilePathView fileName)
+		{
+			@autoreleasepool
+			{
+				const FilePath fullPath = FileSystem::FullPath(fileName);
+
+				if ((not FileSystem::IsFile(fullPath)) && (not FileSystem::IsDirectory(fullPath)))
 				{
 					return false;
 				}
 
-				return detail::MacOS_OpenHTMLInBrowser(Unicode::ToUTF8(FileSystem::FullPath(url)).c_str());
+				const bool isDirectory = FileSystem::IsDirectory(fullPath);
+				NSURL* fileURL = ToFileURL(fullPath, isDirectory);
+
+				return ([[NSWorkspace sharedWorkspace] openURL:fileURL] == YES);
+			}
+		}
+
+		////////////////////////////////////////////////////////////////
+		//
+		//  LaunchFileWithTextEditor
+		//
+		////////////////////////////////////////////////////////////////
+
+		bool LaunchFileWithTextEditor(const FilePathView fileName)
+		{
+			@autoreleasepool
+			{
+				const FilePath fullPath = FileSystem::FullPath(fileName);
+
+				if (not FileSystem::IsFile(fullPath))
+				{
+					return false;
+				}
+
+				NSURL* fileURL = ToFileURL(fullPath, false);
+
+				NSURL* textEditURL =
+					[[NSWorkspace sharedWorkspace] URLForApplicationWithBundleIdentifier:@"com.apple.TextEdit"];
+
+				if (textEditURL == nil)
+				{
+					return false;
+				}
+
+				NSWorkspaceOpenConfiguration* config = [NSWorkspaceOpenConfiguration configuration];
+				config.activates = YES;
+
+				[[NSWorkspace sharedWorkspace] openURLs:@[ fileURL ]
+								withApplicationAtURL:textEditURL
+										configuration:config
+									completionHandler:nil];
+
+				return true;
 			}
 		}
 	}
