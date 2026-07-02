d33734f5150d2ae09440aa95f399d600a2a0ae05
Ryo Suzuki <reputeless+github@gmail.com>
2026-07-03 00:59:34 +0900

    Update PredefinedNamedParameter.hpp

--- Siv3D/include/Siv3D/PredefinedNamedParameter.hpp
+++ Siv3D/include/Siv3D/PredefinedNamedParameter.hpp
@@ -14,31 +14,26 @@
 
 namespace s3d::Arg
 {
-	SIV3D_NAMED_PARAMETER(generator);			// Array, Image, Grid
-	SIV3D_NAMED_PARAMETER(generator0_1);		// Image
-	SIV3D_NAMED_PARAMETER(reserve);				// Array
+	SIV3D_NAMED_PARAMETER(generator);	
+	SIV3D_NAMED_PARAMETER(generator0_1);
+	SIV3D_NAMED_PARAMETER(reserve);
 
-	SIV3D_NAMED_PARAMETER(center);				// Rect, RectF, Circle
-	SIV3D_NAMED_PARAMETER(topLeft);				// Rect, RectF, Circle
-	SIV3D_NAMED_PARAMETER(topRight);			// Rect, RectF, Circle
-	SIV3D_NAMED_PARAMETER(bottomLeft);			// Rect, RectF, Circle
-	SIV3D_NAMED_PARAMETER(bottomRight);			// Rect, RectF, Circle
-	SIV3D_NAMED_PARAMETER(topCenter);			// Rect, RectF, Circle
-	SIV3D_NAMED_PARAMETER(bottomCenter);		// Rect, RectF, Circle
-	SIV3D_NAMED_PARAMETER(middleLeft);			// Rect, RectF, Circle
-	SIV3D_NAMED_PARAMETER(middleRight);			// Rect, RectF, Circle
-	SIV3D_NAMED_PARAMETER(top);					// Rect, RectF
-	SIV3D_NAMED_PARAMETER(bottom);				// Rect, RectF
-	SIV3D_NAMED_PARAMETER(left);				// Rect, RectF
-	SIV3D_NAMED_PARAMETER(right);				// Rect, RectF
-	SIV3D_NAMED_PARAMETER(start);				// Circle
-	SIV3D_NAMED_PARAMETER(end);					// Circle
+	SIV3D_NAMED_PARAMETER(center);
+	SIV3D_NAMED_PARAMETER(topLeft);
+	SIV3D_NAMED_PARAMETER(topRight);
+	SIV3D_NAMED_PARAMETER(bottomLeft);
+	SIV3D_NAMED_PARAMETER(bottomRight);
+	SIV3D_NAMED_PARAMETER(top);
+	SIV3D_NAMED_PARAMETER(bottom);
+	SIV3D_NAMED_PARAMETER(left);
+	SIV3D_NAMED_PARAMETER(right);
+	SIV3D_NAMED_PARAMETER(start);
+	SIV3D_NAMED_PARAMETER(end);
 
-	SIV3D_NAMED_PARAMETER(r);					// Circular
-	SIV3D_NAMED_PARAMETER(theta);				// Circular
+	SIV3D_NAMED_PARAMETER(r);
+	SIV3D_NAMED_PARAMETER(theta);
+	SIV3D_NAMED_PARAMETER(angle);
+	SIV3D_NAMED_PARAMETER(offset);
 
-	SIV3D_NAMED_PARAMETER(angle);				// Line
-	SIV3D_NAMED_PARAMETER(offset);				// Line
-
-	SIV3D_NAMED_PARAMETER(sampleRate);			// Wave
+	SIV3D_NAMED_PARAMETER(sampleRate);
 }
